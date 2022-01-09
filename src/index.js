1c181a7682cd7433373583a56c6ee775618d2d7c
Pablov1999 <verenicpavel32@gmail.com>
2022-01-09 19:27:27 +0300

    come on, Pablo, you got this

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function toReadable (num) {
     var a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
     var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
     
-    
+        if (num === 0) return 'zero';
         if ((num = num.toString()).length > 3) return 'overflow';
         n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
         if (!n) return; var str = '';
