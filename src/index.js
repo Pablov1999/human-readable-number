2026ed24acbe43cabc942925b1ff7f900b924b2f
Pablov1999 <verenicpavel32@gmail.com>
2022-01-09 19:24:01 +0300

    asfasf

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (num) {
+    var a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+    var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+    
+    
+        if ((num = num.toString()).length > 3) return 'overflow';
+        n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+        if (!n) return; var str = '';
+        str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+        str += (n[5] != 0) ? ((str != '') ? '' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + '' : '';
+        return str.trim();
+    
+    
 }
